cb19940c01087cf7ebb20f17c1c8df0fcffb00dc
vishal <vg2556519@gmail.com>
2026-01-26 12:02:12 +0530

    miora bugs

--- my-medusa-store/medusa-config.ts
+++ my-medusa-store/medusa-config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       // Frontend URL: https://oweg-ecommerce.vercel.app/
       // Affiliate Portal: http://localhost:5000
       storeCors: process.env.STORE_CORS || "http://localhost:3000,http://localhost:3001,http://localhost:4000,https://oweg-ecommerce.vercel.app,https://oweg-ecommerce-o3lv.vercel.app",
-      adminCors: process.env.ADMIN_CORS || "http://localhost:7001,https://oweg-ecommerce-o3lv.vercel.app",
+      adminCors: process.env.ADMIN_CORS || "http://localhost:7001,http://localhost:4000,https://oweg-ecommerce-o3lv.vercel.app",
       authCors: process.env.AUTH_CORS || "http://localhost:3000,http://localhost:3001,http://localhost:4000,https://oweg-ecommerce.vercel.app,https://oweg-ecommerce-o3lv.vercel.app",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
